b97a0ff2336f39d6c62bf0afd232dea5cb18f960
mwyzer <muhammad.wyzer@gmail.com>
2022-03-24 08:59:58 +0700

    add bcrypt

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     });
 
-    console.log('MongoDB Connected...');
-  } catch(err) {
+    console.log("MongoDB Connected...");
+  } catch (err) {
     console.log(err.message);
     // Exit process with failure
     process.exit(1);
